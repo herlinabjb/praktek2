d4f0b0b02a5c9ba64b1b40f151a5718d6e3642ea
herlinabjb <32451391+herlinabjb@users.noreply.github.com>
2018-03-31 12:36:52 +0700

    mengisi nilai atribut

--- src/praktek2/jualbajuaksi.java
+++ src/praktek2/jualbajuaksi.java
@@ -2,6 +2,8 @@ package praktek2;
 public class jualbajuaksi {
     public static void main(String[] args) {
         JualBaju rl = new JualBaju();
+        rl.harga_baju = 80000;
+        rl.jumlah_beli = 3;
         
         
     }
