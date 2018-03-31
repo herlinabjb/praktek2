bb269f07a4b76c8ae28db4127d3026a8a259f1b8
herlinabjb <32451391+herlinabjb@users.noreply.github.com>
2018-03-31 12:28:14 +0700

    membuat atribut dan behavior

--- src/praktek2/JualBaju.java
+++ src/praktek2/JualBaju.java
@@ -1,4 +1,12 @@
 package praktek2;
 public class JualBaju {
+    Integer harga_baju;
+    Integer jumlah_beli;
+    
+   void cetakInfo(){
+        System.out.println("harga_baju : "+harga_baju);
+        System.out.println("jumlah_beli : "+jumlah_beli);
+        
+    }
     
 }
